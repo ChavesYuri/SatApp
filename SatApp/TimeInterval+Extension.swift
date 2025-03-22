5516e8d72e0ecaaa53ff19e3c02aacab0af7c2da
ChavesYuri <33982282+ChavesYuri@users.noreply.github.com>
2025-03-22 18:46:11 -0300

    formatted timer value

---
+++ SatApp/TimeInterval+Extension.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension TimeInterval {
+    var formattedTime: String {
+        let hours = Int(self) / 3600
+        let minutes = (Int(self) % 3600) / 60
+        let seconds = Int(self) % 60
+        let milliseconds = Int((self - floor(self)) * 10)
+        
+        if hours > 0 {
+            return String(format: "%d:%02d:%02d.%d", hours, minutes, seconds, milliseconds)
+        } else if minutes > 0 {
+            return String(format: "%02d:%02d.%d", minutes, seconds, milliseconds)
+        } else {
+            return String(format: "%02d.%d", seconds, milliseconds)
+        }
+    }
+}
